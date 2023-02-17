832dad0cf82740a6a3f1f773d70b696e7f6b50e0
John Doe <malachipito@protonmail.com>
2023-02-17 10:00:46 +0100

    Amélioration du front

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './layout/Navbar';
@@ -9,7 +10,6 @@ import AddClient from "./components/AddClient";
 import EditClient from "./components/EditClient";
 import ViewClient from "./components/ViewClient";
 import Footer from './layout/Footer';
-import { GiHelp } from "react-icons/gi";
 
 function App() {
   return (
@@ -25,19 +25,6 @@ function App() {
         </Routes>
       <Footer />
       </Router>
-      <div className="modal fade text-center" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered text-dark" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel"><GiHelp className="h3 pe-2" />Besoin d'aide ?</h5>
-                <button type="button" class="btn-close" aria-label="Close" data-bs-dismiss="modal"></button>
-            </div>
-              <div className="modal-body">
-                <p className="px-3">En cas de problèmes avec l'application, merci de prendre contact avec notre assistance à l'adresse e-mail suivante: <span className="fw-bold line-break">support@peoplecheck.fr</span></p>
-              </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BsFillHouseDoorFill } from 'react-icons/bs';
 import { IoMdHelpCircle } from "react-icons/io";
 import { FaUsers } from 'react-icons/fa';
+import { GiHelp } from "react-icons/gi";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Navbar() {
-  return (
-    <header>
-		<nav className="navbar navbar-dark bg-dark navbar-expand-md text-uppercase fixed-top px-5">
-            <Link className="navbar-brand" to="/"><img src={require('../medias/logo.png')} width="250" alt="PeopleCheck logo" /></Link>
-			<button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="navbar-collapse collapse" id="navbar">
-				<ul className="navbar-nav ms-auto text-end mt-3 mt-md-0 mt-lg-0 mt-xl-0 align-middle">
-					<li className="nav-item"><Link className="nav-link" to="/"><BsFillHouseDoorFill className="h3" /></Link></li>
-					<li className="nav-item"><Link className="nav-link px-md-2 px-lg-2 px-xl-3" to="#"><IoMdHelpCircle className="h3" data-bs-toggle="modal" data-bs-target="#exampleModal" /></Link></li>
-					<li className="nav-item"><Link className="nav-link px-md-2 px-lg-2 px-xl-3" to="/clients"><FaUsers className="h3" /></Link></li>
-				</ul>
-			</div>
-		</nav>
-	</header>
-  )
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	return (
+	<div>
+		<header>
+			<nav className="navbar navbar-dark bg-dark navbar-expand-md text-uppercase fixed-top px-5">
+				<Link className="navbar-brand" to="/"><img src={require('../medias/logo.png')} width="250" alt="PeopleCheck logo" /></Link>
+				<button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="navbar-collapse collapse" id="navbar">
+					<ul className="navbar-nav ms-auto text-end mt-3 mt-md-0 mt-lg-0 mt-xl-0 align-middle">
+						<li className="nav-item"><Link className="nav-link" to="/"><BsFillHouseDoorFill className="h3" /></Link></li>
+						<li className="nav-item"><Link className="nav-link px-md-2 px-lg-2 px-xl-3" to="#"><IoMdHelpCircle className="h3" onClick={handleShow} /></Link></li>
+						<li className="nav-item"><Link className="nav-link px-md-2 px-lg-2 px-xl-3" to="/clients"><FaUsers className="h3" /></Link></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+		<Modal show={show} onHide={handleClose} aria-labelledby="contained-modal-title-vcenter" centered>
+			<Modal.Header closeButton>
+				<Modal.Title><GiHelp className="pe-2" />Besoin d'aide ?</Modal.Title>
+			</Modal.Header>
+			<Modal.Body className="text-center p-4">Vous rencontrez des problèmes avec l'application ? Contactez notre fonction support à l'adresse email suivante: <span className="line-break fw-bold">support@peoplecheck.fr</span></Modal.Body>
+		</Modal>
+	</div>
+	)
 }
 
 export default Navbar
